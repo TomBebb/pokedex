c905aada076a3314944ba98278845f860f24ba87
Tom Bebbington <tophattedcoder@gmail.com>
2024-05-17 20:03:29 +0100

    Support nav categories

--- src/nav.ts
+++ src/nav.ts
@@ -1,29 +1,76 @@
-export type 
+export const navCategories = {
+  berries: "Berries",
+  contests: "Contests",
+  encounters: "Encounters",
+  evolutions: "Evolutions",
+  games: "Games",
+  items: "Items",
+  locations: "Locations",
+  machines: "Machines",
+  moves: "Machines",
+  pokemon: "Pokémon",
+}
+export type NavCategory = keyof typeof navCategories
+
 export interface NavItem {
   name: string
   path: string
   desc: string
+  category: NavCategory
 }
 
 export const navItems: NavItem[] = [
   {
+    path: "/list/item",
+    name: "Items",
+    desc: "View a listing of all items",
+    category: "items",
+  },
+  {
+    path: "/list/item-category",
+    name: "Item Categories",
+    desc: "View a listing of all item categories",
+    category: "items",
+  },
+  {
+    path: "/list/berry",
+    name: "Berries",
+    desc: "View a listing of all berrys",
+    category: "berries",
+  },
+  {
     path: "/list/pokemon",
-    name: "Pokemon",
+    name: "Pokémon",
     desc: "View a listing of all Pokemon",
+    category: "pokemon",
   },
   {
     path: "/list/move",
-    name: "Pokemon Moves",
+    name: "Moves",
     desc: "View a listing of all Pokemon moves",
+    category: "moves",
+  },
+  {
+    path: "/list/generation",
+    name: "Generations",
+    desc: "View a listing of all Pokemon game generations",
+    category: "games",
   },
   {
     path: "/list/version",
     name: "Pokemon Games",
     desc: "View a listing of all Pokemon games",
+    category: "games",
   },
   {
     path: "/list/machines",
     name: "Pokemon Machines",
     desc: "View a listing of all Pokemon machines: TMs, HMs etc",
+    category: "machines",
   },
 ]
+export const navItemsByCat: Record<NavCategory, NavItem[]> = Object.fromEntries(
+  Object.keys(navCategories)
+    .map((cat) => [cat, navItems.filter((ni) => ni.category === cat)])
+    .filter(([k, v]) => v.length)
+)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
-import { navItems } from "../nav.ts"
+import { navItemsByCat, navCategories } from "../nav.ts"
 
 import { RouterLink } from "vue-router"
 </script>
 
 <template>
-  <div class="grid gap-8 p-8 lg:grid-cols-3">
-    <RouterLink v-for="item in navItems" :to="item.path"
-      class="flex flex-col items-center gap-8 rounded-lg border-2 p-4">
-      <div class="text-2xl">
-        {{ item.name }}
+  <div class="flex flex-col  p-8 gap-2">
+    <div v-for="(navItems, navKey) in navItemsByCat ">
+      <div class="text-2xl font-bold text-center">
+        {{ navCategories[navKey] }}
       </div>
-      <div class="text-ellipsis text-center text-xl">
-        {{ item.desc }}
+      <div class="grid gap-8 lg:grid-cols-3">
+        <RouterLink v-for="item in navItems" :to="item.path"
+          class="flex flex-col items-center gap-8 rounded-lg border-2 p-4">
+          <div class="text-2xl">
+            {{ item.name }}
+          </div>
+          <div class="text-ellipsis text-center text-xl">
+            {{ item.desc }}
+          </div>
+        </RouterLink>
       </div>
-    </RouterLink>
+    </div>
   </div>
 </template>
