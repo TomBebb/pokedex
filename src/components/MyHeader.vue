494ff12cfcee29894d9a4f5cd94779d0e1e458f1
Tom Bebbington <tophattedcoder@gmail.com>
2024-05-17 18:48:27 +0100

    Link to main

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
 defineProps<{ pageName: string }>()
 </script>
 <template>
-  <header
-    class="sticky top-0 flex h-[6vh] select-none flex-row items-center gap-8 bg-gray-800 px-2 py-1 text-white"
-  >
+  <header class="sticky top-0 flex h-[6vh] select-none flex-row items-center gap-8 bg-gray-800 px-2 py-1 text-white">
+    <RouterLink to="/" class="text-xl">PokeDex</RouterLink>
     <div class="text-xl font-bold">
       {{ pageName }}
     </div>
-    <div class="text-xl">-</div>
-    <div class="text-xl">PokeDex</div>
   </header>
 </template>
