9b3f4d37735128ebae32f4adefbf5a097cf8101a
Tom Bebbington <tophattedcoder@gmail.com>
2024-05-17 22:34:10 +0100

    Add github link in corner

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,15 @@
 import { RouterView } from "vue-router"
 import MyHeader from "./components/MyHeader.vue"
 import { getRouteName } from "./router"
+import { Icon } from "@iconify/vue"
 </script>
 
 <template>
+  <div class="fixed bottom-0 right-0 text-3xl p-1 flex flex-row">
+    <a href="https://github.com/TomBebb/pokedex">
+      <Icon icon="fa-brands:github" />
+    </a>
+  </div>
   <div class="font-sans">
     <MyHeader :page-name="getRouteName($route)" />
     <RouterView />
