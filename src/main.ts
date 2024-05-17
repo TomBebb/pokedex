cabe4fd6452f7948504151403666bc4a051770e1
Tom Bebbington <tophattedcoder@gmail.com>
2024-05-17 17:28:31 +0100

    Auto-change tab title

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue"
 import "./index.css"
 import App from "./App.vue"
-import { router } from "./router"
+import { router, getRouteName } from "./router"
 
 createApp(App).use(router).mount("#app")
+router.beforeEach((to) => {
+  document.title = getRouteName(to as any) + " - PokeDex"
+})
