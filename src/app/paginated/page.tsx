797d4d74f6d7a2e20d9dd424d024d12b2e212959
Tom Bebbington <tophattedcoder@gmail.com>
2024-07-17 17:40:50 +0100

    Support basic next advancement

--- src/app/paginated/page.tsx
+++ src/app/paginated/page.tsx
@@ -3,9 +3,28 @@ import PokemonTypeDisplay from "@/app/components/PokemonTypeDisplay"
 import "@/app/globals.css"
 import { Paginated, Pokemon } from "@/common/models"
 import { useFetch } from "@mantine/hooks"
+import Link from "next/link"
+import { useSearchParams } from "next/navigation"
+import { useMemo } from "react"
 
 export default function PaginatedPage() {
-  const { data } = useFetch<Paginated<Pokemon>>("/api/paginated")
+  const params = useSearchParams()
+  const { data } = useFetch<Paginated<Pokemon>>("/api/paginated?" + params)
+  const hasNext = useMemo(
+    () => data !== null && data.offset + data.items.length < data.total,
+    [data]
+  )
+  const nextLink = useMemo<string | null>(
+    () =>
+      hasNext
+        ? "/paginated?" +
+          new URLSearchParams({
+            limit: data!.limit!.toString(),
+            offset: (data!.offset! + data!.limit!).toString(),
+          })
+        : null,
+    [data, hasNext]
+  )
   return data ? (
     <div className="grid grid-cols-2">
       {data.items.map((p: Pokemon) => (
@@ -19,6 +38,14 @@ export default function PaginatedPage() {
           </div>
         </>
       ))}
+
+      <div className="col-span-2">
+        {nextLink && (
+          <Link href={nextLink} className="p-2  outline-3">
+            Next
+          </Link>
+        )}
+      </div>
     </div>
   ) : (
     <div>Loading...</div>
