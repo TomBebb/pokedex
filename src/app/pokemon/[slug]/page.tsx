0522e4726c525ef80467a1c2d3e6c76de58c7104
Tom Bebbington <tophattedcoder@gmail.com>
2024-07-19 00:54:15 +0100

    Show pokemon sprite

--- src/app/pokemon/[slug]/page.tsx
+++ src/app/pokemon/[slug]/page.tsx
@@ -9,24 +9,34 @@ export default function PokemonView() {
   const { slug } = useParams<{ slug: string }>()!
   const { data } = useFetch<Pokemon>("/api/" + slug)
   return data !== null ? (
-    <div className="flex flex-col">
-      <KeyValueView name="Name">{data!.name}</KeyValueView>
-      <KeyValueView name="Types">
-        <div className="w-full flex flex-row gap-5 justify-center lg:justify-start">
-          {data.types.map((ty) => (
-            <PokemonTypeDisplay key={ty} value={ty} />
-          ))}
-        </div>
-      </KeyValueView>
-      <KeyValueView name="Height">{data.height}</KeyValueView>
-      <KeyValueView name="Weight">{data.weight}</KeyValueView>
-      <KeyValueView name="Generation">{data.gen}</KeyValueView>
-      <KeyValueView name="HP">{data.baseStats.hp}</KeyValueView>
-      <KeyValueView name="Attack">{data.baseStats.attack}</KeyValueView>
-      <KeyValueView name="Defense">{data.baseStats.defense}</KeyValueView>
-      <KeyValueView name="Special Attack">{data.baseStats.spAtk}</KeyValueView>
-      <KeyValueView name="Special Defense">{data.baseStats.spDef}</KeyValueView>
-      <KeyValueView name="Speed">{data.baseStats.speed}</KeyValueView>
+    <div className="flex  flex-col lg:flex-row">
+      <img
+        className="object-cover pixelated max-h-[25vw]"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/172.png`}
+      />
+      <div className="flex flex-col flex-1">
+        <KeyValueView name="Name">{data!.name}</KeyValueView>
+        <KeyValueView name="Types">
+          <div className="w-full flex flex-row gap-5 justify-center lg:justify-start">
+            {data.types.map((ty) => (
+              <PokemonTypeDisplay key={ty} value={ty} />
+            ))}
+          </div>
+        </KeyValueView>
+        <KeyValueView name="Height">{data.height}</KeyValueView>
+        <KeyValueView name="Weight">{data.weight}</KeyValueView>
+        <KeyValueView name="Generation">{data.gen}</KeyValueView>
+        <KeyValueView name="HP">{data.baseStats.hp}</KeyValueView>
+        <KeyValueView name="Attack">{data.baseStats.attack}</KeyValueView>
+        <KeyValueView name="Defense">{data.baseStats.defense}</KeyValueView>
+        <KeyValueView name="Special Attack">
+          {data.baseStats.spAtk}
+        </KeyValueView>
+        <KeyValueView name="Special Defense">
+          {data.baseStats.spDef}
+        </KeyValueView>
+        <KeyValueView name="Speed">{data.baseStats.speed}</KeyValueView>
+      </div>
     </div>
   ) : (
     <>Loading...</>
