e16e110f600ee9f199c0507dd177f2f41859b483
Tom Bebbington <tophattedcoder@gmail.com>
2024-07-19 20:51:14 +0100

    Support keyvalueview classname

--- src/app/components/KeyValueView.tsx
+++ src/app/components/KeyValueView.tsx
@@ -1,10 +1,11 @@
 export interface KeyValueViewProps {
   name: JSX.Element | string
   children: JSX.Element | string | number
+  className?: string
 }
 export default function KeyValueView(props: KeyValueViewProps): JSX.Element {
   return (
-    <div className="grid grid-cols-3 gap-2 lg:gap-0">
+    <div className={`grid grid-cols-3 gap-2 lg:gap-0 ${props.className}`}>
       <div className="px-2 col-span-3 lg:col-span-1 font-bold outline outline-1 text-center lg:text-right">
         {props.name}
       </div>
