3d106713c660b7057fcbfe4740716ef237610da1
Tom Bebbington <tophattedcoder@gmail.com>
2024-07-19 20:42:29 +0100

    Remove keyvalview desktop gap

--- src/app/components/KeyValueView.tsx
+++ src/app/components/KeyValueView.tsx
@@ -4,7 +4,7 @@ export interface KeyValueViewProps {
 }
 export default function KeyValueView(props: KeyValueViewProps): JSX.Element {
   return (
-    <div className="grid grid-cols-3 gap-2">
+    <div className="grid grid-cols-3 gap-2 lg:gap-0">
       <div className="px-2 col-span-3 lg:col-span-1 font-bold outline outline-1 text-center lg:text-right">
         {props.name}
       </div>
