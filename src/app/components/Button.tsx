afe21f7273aa545947be1d68e02236a21a85ceb7
Tom Bebbington <tophattedcoder@gmail.com>
2024-07-19 20:25:32 +0100

    Support button disabling

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -2,17 +2,20 @@ import React from "react"
 
 import "@/app/globals.css"
 import Link from "next/link"
+import { Url } from "node:url"
 export interface ButtonProps {
   children: React.ReactNode
   className?: string
   href?: string
+  disabled?: boolean
 }
 export default function Button({ children, ...props }: ButtonProps) {
-  const El = props.href ? Link : "button"
+  const El = props.href && !props.disabled ? Link : "button"
   return (
     <El
-      href={props.href ?? ""}
-      className={`outline-1 outline outline-blue-950 text-gray-950 px-2 py-0.5 bg-blue-300 hover:bg-blue-600 transition-colors rounded ${props.className}`}
+      href={(props.disabled ? undefined : (props.href ?? "")) as unknown as Url}
+      disabled={props.disabled}
+      className={`outline-1 outline ${props.disabled ? "bg-gray-300" : "bg-blue-300 hover:bg-blue-600"} outline-blue-950 text-gray-950 px-2 py-0.5 transition-colors rounded ${props.className}`}
     >
       {children}
     </El>
