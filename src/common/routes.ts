89d5bc32473fe003adfb986e4b67d4e9376c4db2
Tom Bebbington <tophattedcoder@gmail.com>
2024-07-19 23:11:32 +0100

    Add route matcher for regex paths

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -1,12 +1,24 @@
+"use client"
+import { usePathname } from "next/navigation"
+
 export interface Route {
   name: string
-  path: string
+  path: string | RegExp
+  description?: string
+  hidden?: boolean
   icon?: {
     active: string
     inactive: string
   }
 }
-
+export function routePathMatch(
+  curr: string,
+  routePath: Route["path"]
+): boolean {
+  return typeof routePath === "string"
+    ? routePath === curr
+    : routePath.test(curr)
+}
 function antIcon(name: string): Route["icon"] {
   return {
     active: `ant-design:${name}-filled`,
@@ -32,5 +44,16 @@ export const routes: Route[] = [
     name: "List Pokemon",
     path: "/paginated",
     icon: outlinedAntIcon("ordered-list"),
+    description: "List all the Pokemon available",
+  },
+  {
+    hidden: true,
+    name: "Pokemon",
+    path: /^\/pokemon\/[0-9]+$/gm,
+    icon: outlinedAntIcon("ordered-list"),
   },
 ]
+export function useActiveRoute(): Route | undefined {
+  const pathname = usePathname() ?? ""
+  return routes.find((route) => routePathMatch(pathname, route.path))
+}
